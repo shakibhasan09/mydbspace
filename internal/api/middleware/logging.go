b8f0c2b6360fee6cb14d3e853a216c492bb07b7d
Sakib Hasan <shakibhasan.me09@gmail.com>
2025-02-11 19:11:19 +0600

    added middlewares

---
+++ internal/api/middleware/logging.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		next.ServeHTTP(w, r)
+
+		duration := time.Since(start)
+		log.Printf("%s %s %d %s", r.Method, r.URL.Path, http.StatusOK, duration)
+	})
+}
