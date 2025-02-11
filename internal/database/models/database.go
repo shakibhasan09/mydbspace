34f7f0378e98138f2adbc1b5727c664274a275f0
Sakib Hasan <shakibhasan.me09@gmail.com>
2025-02-11 20:45:08 +0600

    added database model

---
+++ internal/database/models/database.go
@@ -0,0 +1,16 @@
+package models
+
+type Database struct {
+	Uuid        string `json:"uuid"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+	Host        string `json:"host"`
+	Port        int    `json:"port"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Database    string `json:"database"`
+	SSLMode     string `json:"sslmode"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
