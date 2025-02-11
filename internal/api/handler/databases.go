e27dd319cbdd0c689434025b5ec606104639ffbd
Sakib Hasan <shakibhasan.me09@gmail.com>
2025-02-11 20:10:09 +0600

    added database handlers

---
+++ internal/api/handler/databases.go
@@ -0,0 +1,23 @@
+package handler
+
+import "net/http"
+
+func GetDatabases(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("GetDatabases"))
+}
+
+func CreateDatabase(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("CreateDatabase"))
+}
+
+func GetDatabase(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("GetDatabase"))
+}
+
+func UpdateDatabase(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("UpdateDatabase"))
+}
+
+func DeleteDatabase(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("DeleteDatabase"))
+}
