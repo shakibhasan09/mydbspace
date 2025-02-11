c23aeabb7c61c93ee766907872b15aa13d38cfe1
Sakib Hasan <shakibhasan.me09@gmail.com>
2025-02-11 19:11:01 +0600

    fix 404 error for api

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/shakibhasan09/mydbspace/internal/api/middleware"
 )
 
 //go:embed web/dist/*
@@ -21,17 +23,22 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if !strings.HasPrefix(r.URL.Path, "/api/") {
-			_, err := fs.Stat(spaFS, strings.TrimPrefix(r.URL.Path, "/"))
-			if errors.Is(err, fs.ErrNotExist) {
-				r.URL.Path = "/"
-			}
-
-			http.FileServer(http.FS((spaFS))).ServeHTTP(w, r)
+		if strings.HasPrefix(r.URL.Path, "/api/") {
+			http.NotFound(w, r)
 			return
 		}
+
+		_, err := fs.Stat(spaFS, strings.TrimPrefix(r.URL.Path, "/"))
+		if errors.Is(err, fs.ErrNotExist) {
+			r.URL.Path = "/"
+		}
+
+		http.FileServer(http.FS(spaFS)).ServeHTTP(w, r)
+
 	})
 
+	handler := middleware.LoggingMiddleware(middleware.AuthMiddleware(mux))
+
 	log.Println("Server running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
