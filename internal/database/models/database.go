768510bdb791beb3730ad028a1942893b0b8356b
Sakib Hasan <shakibhasan.me09@gmail.com>
2025-02-12 16:38:59 +0600

    added image name field

--- internal/database/models/database.go
+++ internal/database/models/database.go
@@ -11,6 +11,7 @@ type Database struct {
 	Password    string `json:"password"`
 	Database    string `json:"database"`
 	SSLMode     string `json:"sslmode"`
+	Image       string `json:"image"`
 	CreatedAt   string `json:"created_at"`
 	UpdatedAt   string `json:"updated_at"`
 }
