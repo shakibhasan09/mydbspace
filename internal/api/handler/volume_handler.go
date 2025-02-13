e0cf43c27d8a5fa7213a8cd26577bfde59561edf
Sakib Hasan <shakibhasan.me09@gmail.com>
2025-02-14 02:23:13 +0600

    updated volume handlers

--- internal/api/handler/volume_handler.go
+++ internal/api/handler/volume_handler.go
@@ -28,12 +28,7 @@ func CreateVolume(c *fiber.Ctx) error {
 
 	// TODO: Validate body
 
-	volume, err := db.NamedExec("INSERT INTO volumes (uuid, key, name) VALUES (:uuid, :key, :name)", &models.Volume{Uuid: uuid.New().String(), Key: body.Key, Name: body.Name})
-	if err != nil {
-		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
-	}
-
-	if _, err := volume.LastInsertId(); err != nil {
+	if _, err := db.NamedExec("INSERT INTO volumes (uuid, key, name) VALUES (:uuid, :key, :name)", &models.Volume{Uuid: uuid.New().String(), Key: body.Key, Name: body.Name}); err != nil {
 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
 
@@ -43,6 +38,11 @@ func CreateVolume(c *fiber.Ctx) error {
 func DeleteVolume(c *fiber.Ctx) error {
 	db := database.GetDB()
 
+	volume := models.Volume{}
+	if err := db.Get(&volume, "SELECT * FROM volumes WHERE uuid = ?", c.Params("uuid")); err != nil {
+		return fiber.NewError(fiber.StatusNotFound, err.Error())
+	}
+
 	if _, err := db.Exec("DELETE FROM volumes WHERE uuid = ?", c.Params("uuid")); err != nil {
 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
@@ -51,7 +51,25 @@ func DeleteVolume(c *fiber.Ctx) error {
 }
 
 func UpdateVolume(c *fiber.Ctx) error {
-	return c.SendString("Hello, World!")
+	db := database.GetDB()
+
+	body := models.Volume{}
+	if err := c.BodyParser(&body); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	// TODO: Validate body
+
+	volume := models.Volume{}
+	if err := db.Get(&volume, "SELECT * FROM volumes WHERE uuid = ?", c.Params("uuid")); err != nil {
+		return fiber.NewError(fiber.StatusNotFound, err.Error())
+	}
+
+	if _, err := db.Exec("UPDATE volumes SET key = ?, name = ? WHERE uuid = ?", body.Key, body.Name, c.Params("uuid")); err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(fiber.Map{"message": "Volume updated successfully"})
 }
 
 func GetVolume(c *fiber.Ctx) error {
