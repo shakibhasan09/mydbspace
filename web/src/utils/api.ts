897918e74c01bee07f868778edc6ccc6d1156f55
Sakib Hasan <shakibhasan.me09@gmail.com>
2025-02-16 04:33:56 +0600

    remove  auth header

--- web/src/utils/api.ts
+++ web/src/utils/api.ts
@@ -1,7 +1,6 @@
 export const api = (path: string, init?: RequestInit) => {
   return fetch(`http://localhost:3000${path}`, {
     headers: {
-      Authorization: `Basic ${btoa("admin:123456")}`,
       "Content-Type": "application/json",
     },
     credentials: "include",
