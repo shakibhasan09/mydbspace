7d192d590802080d084aa91cf384373c5ec3cae9
Sakib Hasan <shakibhasan.me09@gmail.com>
2025-02-16 14:17:36 +0600

    updated

--- web/src/components/databases/view.tsx
+++ web/src/components/databases/view.tsx
@@ -57,39 +57,53 @@ export const DatabasesView = (props: DatabasesViewProps) => {
               : {props.database.image_version}
             </p>
           </div>
+          <div className="grid grid-cols-6 gap-4">
+            <h1 className="col-span-2 text-muted-foreground">
+              Database Public Port
+            </h1>
+            <p className="text-base col-span-4">: {props.database.port}</p>
+          </div>
+          <div className="grid grid-cols-6 gap-4">
+            <h1 className="col-span-2 text-muted-foreground">
+              Database Domain
+            </h1>
+            <p className="text-base col-span-4">: {props.database.domain}</p>
+          </div>
         </CardContent>
       </Card>
-      <Card className="bg-muted/50">
-        <CardHeader className="flex items-center gap-2 flex-row space-y-0">
-          <Activity className="h-5 w-5 text-primary" />
-          <h3 className="font-semibold text-lg">Database Status</h3>
-        </CardHeader>
-        <CardContent className="space-y-2">
-          <div className="flex items-center justify-between">
-            <span className="text-muted-foreground">Status</span>
-            <div className="flex items-center gap-2">
-              <Circle className="size-4 fill-green-500 text-green-500 animate-pulse" />
-              <span className="font-medium uppercase">
-                {props.database.status}
-              </span>
+      <div>
+        <Card className="bg-muted/50">
+          <CardHeader className="flex items-center gap-2 flex-row space-y-0">
+            <Activity className="h-5 w-5 text-primary" />
+            <h3 className="font-semibold text-lg">Database Status</h3>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="flex items-center justify-between">
+              <span className="text-muted-foreground">Status</span>
+              <div className="flex items-center gap-2">
+                <Circle className="size-4 fill-green-500 text-green-500 animate-pulse" />
+                <span className="font-medium uppercase">
+                  {props.database.status}
+                </span>
+              </div>
             </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-muted-foreground">Uptime</span>
-            <span className="font-medium">15 days</span>
-          </div>
-        </CardContent>
-        <Separator />
-        <CardFooter className="pt-7 gap-2 justify-end">
-          <Button variant="outline">Restart Database</Button>
-          <Button variant="destructive">
-            <Pause /> Stop Database
-          </Button>
-          <Button variant="destructive" size="icon">
-            <Trash />
-          </Button>
-        </CardFooter>
-      </Card>
+            <div className="flex items-center justify-between">
+              <span className="text-muted-foreground">Uptime</span>
+              <span className="font-medium">15 days</span>
+            </div>
+          </CardContent>
+          <Separator />
+          <CardFooter className="pt-7 gap-2 justify-end">
+            <Button variant="outline">Restart Database</Button>
+            <Button variant="destructive">
+              <Pause /> Stop Database
+            </Button>
+            <Button variant="destructive" size="icon">
+              <Trash />
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
       {envs.length > 0 && (
         <Card className="bg-muted/50 col-span-2">
           <CardHeader className="space-y-0 flex flex-row items-center justify-between">
