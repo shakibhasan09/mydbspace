dc35221425f8bdd9a6b0feb36b058e02e0381cfd
Sakib Hasan <shakibhasan.me09@gmail.com>
2025-02-16 17:54:05 +0600

    added ALLOWED_ORIGINS env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,7 +25,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
+		AllowOrigins:     os.Getenv("ALLOWED_ORIGINS"),
 		AllowCredentials: true,
 	}))
 
